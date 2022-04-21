ea52719b298057aefdcbff4e80d9ea332be4428d
Wdainoski <98496324+Wdainoski@users.noreply.github.com>
2022-04-21 16:53:21 -0400

    project

---
+++ end.js
@@ -0,0 +1,7 @@
+const username = document.querySelector(`#username`)
+const saveScoreBtn = document.querySelector(`#saveScoreBtn`)
+const finalScore = document.querySelector(`#finalScore`)
+const mostRecentScore = document.querySelector(`#mostRecentScore`)
+
+finalScore.innertText= mostRecentScore
+
